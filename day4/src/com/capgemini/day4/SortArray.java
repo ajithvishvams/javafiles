25595970b1e0faec35fa7446df42057615d8510b
ajithvishvams <ajithvishvams19@gmail.com>
2022-02-01 15:06:04 +0530

    date 2/1/2022

---
+++ day4/src/com/capgemini/day4/SortArray.java
@@ -0,0 +1,24 @@
+package com.capgemini.day4;
+public class SortArray 
+{
+	public static void main(String[] args)   
+	{
+	int[] arr = new int[] {10 , 20 , 25 , 5 , 35 , 70 , 1};  
+	System.out.println("Array elements after sorting:");  
+	for (int i = 0; i < arr.length; i++)   
+	{  
+	for (int j = i + 1; j < arr.length; j++)   
+	{  
+	int tmp = 0;  
+	if (arr[i] > arr[j])   
+	{  
+	tmp = arr[i];  
+	arr[i] = arr[j];  
+	arr[j] = tmp;  
+	}  
+	}  
+	System.out.println(arr[i]);  
+	}  
+	}  
+
+}
