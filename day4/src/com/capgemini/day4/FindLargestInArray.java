0f95eba80ba86e85268b81ca52b398f64522d2c2
ajithvishvams <ajithvishvams19@gmail.com>
2022-02-01 14:36:34 +0530

    date 2/1/2022

---
+++ day4/src/com/capgemini/day4/FindLargestInArray.java
@@ -0,0 +1,32 @@
+package com.capgemini.day4;
+
+import java.util.Scanner;
+
+public class FindLargestInArray 
+{
+	public static void main(String[] args) 
+	{  
+
+		{  
+			 int n;
+			Scanner sc=new Scanner(System.in);  
+			System.out.print("Enter the number of elements: ");  
+			n=sc.nextInt();  
+				
+			int[] a1 = new int[10];  	
+			System.out.println("Enter the elements of the array: ");  
+			for(int i=0; i<n; i++)  
+			{  
+
+				a1[i]=sc.nextInt();  
+			}
+		int max = a1[0];
+        for (int i = 0; i <n; i++) {  
+           if(a1[i] > max)  
+               max = a1[i]; 
+        }  
+        System.out.println("Largest element present in given array: " + max);  
+    }
+}
+}
+
