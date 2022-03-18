5cf9e1e367b38600c6ec58ce5eb1f19015071f88
Ajithvishva M S <98728961+ajithvishvams@users.noreply.github.com>
2022-03-18 10:33:31 +0530

    18/03/2022

---
+++ OnlineFlightTicketBookingSystem-PassengerModule/src/main/java/com/capg/springboot/repository/PassengerRepository.java
@@ -0,0 +1,10 @@
+package com.capg.springboot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.capg.springboot.entity.Passenger;
+
+public interface PassengerRepository extends JpaRepository<Passenger,String>{
+	Passenger findByPassengerId(int passengerId);
+
+}
