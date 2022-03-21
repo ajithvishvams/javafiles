9e9b7460d1aaa716bd013c73914c8b7020ea121c
Ajithvishva M S <98728961+ajithvishvams@users.noreply.github.com>
2022-03-21 12:09:43 +0530

    Delete OnlineFlightTicketBookingSystem-PassengerModule directory

--- OnlineFlightTicketBookingSystem-PassengerModule/src/main/java/com/capg/springboot/exceptions/PassengerNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.capg.springboot.exceptions;
-
-public class PassengerNotFoundException extends Exception {
-
-	public PassengerNotFoundException(String s) {
-		super();//need to check
-	}
-}
